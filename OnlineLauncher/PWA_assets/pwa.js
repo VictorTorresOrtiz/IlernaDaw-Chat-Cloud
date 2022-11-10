bc33db20c20eb1e8e3315c4639af93288f92c2d7
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2022-11-10 10:52:50 +0100

    PWA Ready

---
+++ OnlineLauncher/PWA_assets/pwa.js
@@ -0,0 +1,10 @@
+// Service Worker Register 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('sw.js')
+            .then(registration => {
+            })
+            .catch(err => {
+            });
+    });
+}
