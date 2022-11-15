4c27f0ba46fd4fa8c171c70e710a3bd3a48e9345
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2022-11-15 09:47:01 +0100

    Adding Features

--- OnlineLauncher/profile.php
+++ OnlineLauncher/profile.php
@@ -71,7 +71,7 @@
             <div class="col-12">
                 <nav class="main-nav">
                     <!-- ***** Logo Start ***** -->
-                    <a href="index.html" class="logo">
+                    <a href="index.php" class="logo">
                         <img src="assets/images/logo.png" alt="">
                     </a>
                     <!-- ***** Logo End ***** -->
